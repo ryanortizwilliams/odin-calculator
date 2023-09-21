3ef4986c1a151c3094ab4472ced0fa141f1ffe12
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-09-21 16:12:41 +0900

    Change data type of buttons from number to string. Allow multidigit numbers

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 // TODO: add listeners to each button 
 // Create add number function
 
-let num_1 = 0;
-let num_2 = 0;
+let num_1 = "";
+let num_2 = "";
 let operator = "";
 
 
 function toDisplay(numButton) {
-    num_2 = num_1
-    num_1 = numButton;
+    num_1 += numButton;
+
+    //get current display
     let displayNumber = document.getElementById('current-number');
     let newDisplayNumber = document.createElement("h1");
+    // replace with new display
     newDisplayNumber.setAttribute('id', 'current-number');
     newDisplayNumber.textContent = `${num_1}`;
     displayNumber.replaceWith(newDisplayNumber);
@@ -23,33 +25,33 @@ function chooseOperator(operatorButton) {
 //DOM elements
 //numbers
 const zero = document.getElementById("0");
-zero.addEventListener('click', toDisplay.bind(null, 1));
+zero.addEventListener('click', toDisplay.bind(null, "0"));
 const one = document.getElementById("1");
-one.addEventListener('click', toDisplay.bind(null, 1));
+one.addEventListener('click', toDisplay.bind(null, "1"));
 
 const two = document.getElementById("2");
-two.addEventListener('click', toDisplay.bind(null, 2));
+two.addEventListener('click', toDisplay.bind(null, "2"));
 
 const three = document.getElementById("3");
-three.addEventListener('click', toDisplay.bind(null, 3));
+three.addEventListener('click', toDisplay.bind(null, "3"));
 
 const four = document.getElementById("4");
-four.addEventListener('click', toDisplay.bind(null, 4));
+four.addEventListener('click', toDisplay.bind(null, "4"));
 
 const five = document.getElementById("5");
-five.addEventListener('click', toDisplay.bind(null, 5));
+five.addEventListener('click', toDisplay.bind(null, "5"));
 
 const six = document.getElementById("6");
-six.addEventListener('click', toDisplay.bind(null, 6));
+six.addEventListener('click', toDisplay.bind(null, "6"));
 
 const seven = document.getElementById("7");
-seven.addEventListener('click', toDisplay.bind(null, 7));
+seven.addEventListener('click', toDisplay.bind(null, "7"));
 
 const eight = document.getElementById("8");
-eight.addEventListener('click', toDisplay.bind(null, 8));
+eight.addEventListener('click', toDisplay.bind(null, "8"));
 
 const nine = document.getElementById("9");
-nine.addEventListener('click', toDisplay.bind(null, 9));
+nine.addEventListener('click', toDisplay.bind(null, "9"));
 
 //operators
 let plus = document.getElementById("plus");
@@ -78,3 +80,8 @@ function evaluate() {
         break;
     }
 }
+
+//buttons should pass a string
+// after an operation button is pressed, pass num 1 to num 2
+// if num2 exists, pressing a button should immediately reset the screen. 
+// if num2 doesnt exist yet, the first number should stay
